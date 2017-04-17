27cf95fcce8a76841c75ae7811eb9fdb49a53020
Jencir Lee <jli05@icloud.com>
2017-04-17 18:03:28 +0100

    refactor to provide primitives for distributed mode computation

--- cumulants.py
+++ cumulants.py
@@ -1,163 +1,377 @@
 ''' Cumulants computations in local mode
 
-'''
+It computes M1, the product of M2 with a test matrix, and whitening
+of M3, over partitioned documents.
 
+`contrib_xxx()` computes contribution of the current partition
+to the moment, the product of the moment with a test matrix, or
+whitened moment.
+
+`xxx_adj()` computes the list of adjustment terms for the product
+of the moment with a test matrix, or whitened moment.
+'''
+from functools import reduce
+from operator import add
 import numpy as np
 import scipy.sparse as spsp
 
-def mult_E2_X(docs, X, n_docs):
-    ''' Compute the product of E2 and test matrix X
+
+def equal_partitions(n_docs, n_partitions):
+    ''' Compute partition ranges over [0, n_docs)
+
+    Returns an iterator over (start, end) tuples such that
+    range(start, end) marks the index range of each partition.
 
     Parameters
     -----------
-    docs : m-by-V csr_matrix
-        m word count vectors, where V is the vocabulary size,
-        m could be smaller than the total number of documents
-    X : V-by-k array
-        where k is the number of factors
-    n_docs : integer
-        The total number of documents
+    n_docs : int
+        Total number of documents.
+
+    Returns
+    -----------
+    out : iterator over (start, end) tuples
+        Iterator over tuples such that range(start, end) marks
+        the index range of each partition.
+    '''
+    assert n_docs >= 1 and n_partitions >= 1
+
+    starts = [i * (n_docs // n_partitions) for i in range(n_partitions)]
+    ends = starts[1:] + [n_docs]
+    return zip(starts, ends)
+
+# ================= M1 Calculation =================
+def contrib_m1(docs, n_docs):
+    ''' Compute contribution of the partition to M1
+
+    Parameters
+    -----------
+    docs : m-by-vocab_size array or csr_matrix
+        Current partition of word count vectors.
+    n_docs : int
+        total number of documents
 
     Returns
     ----------
-    out : V-by-k csr_matrix
-        Sum of the contributions of each document to E2
+    out : length-V array
+        contribution of `docs` to M1
     '''
-    l = np.squeeze(np.array(docs.sum(axis=1)))
-    diag_l = spsp.diags(1.0 / l / (l - 1))
+    partition_size, vocab_size = docs.shape
+    assert partition_size >= 1 and vocab_size >= 1
+    assert n_docs >= partition_size
 
-    scaled_D = diag_l.dot(docs)
-    t1 = scaled_D.T.dot(docs.dot(X))
+    # transposed normalised docs
+    _docs = docs.T / docs.sum(axis=1)
+    _docs = _docs.T
 
-    sum_scaled_D = np.squeeze(np.array(scaled_D.sum(axis=0)))
-    t2 = spsp.diags(sum_scaled_D).dot(X)
+    assert np.allclose(_docs.sum(axis=1), 1)
+    return _docs.mean(axis=0) * (len(docs) / n_docs)
 
-    return (t1 - t2) / n_docs
+def moment1(docs, n_partitions=1):
+    ''' Compute M1 over partitioned documents
 
-def mult_M2_X_helper(prod_E2_X, X, M1, alpha0):
-    ''' Compute the product of M2 by X
+    Compute over partitioned documents in local mode, mostly used
+    for test purpose.
 
+    Parameters
+    -----------
+    docs : n_docs-by-vocab_size array or csr_matrix
+        The entire collection of word count vectors.
+    n_partitions : int
+        Number of partitions over the document collection, >= 1.
+
+    Returns
+    ----------
+    out : length-vocab_size array
+        M1 of the entire document collection
     '''
-    prod_M1_X = alpha0 / (alpha0 + 1) * np.outer(M1, M1.dot(X))
-    return prod_E2_X - prod_M1_X
+    n_docs, vocab_size = docs.shape
+    assert n_docs >= 1 and vocab_size >= 1
+    assert n_partitions >= 1 and n_partitions <= n_docs
 
-def mult_M2_X(docs, X, n_docs, M1, alpha0):
-    ''' Compute the product of M2 by test matrix X in local mode
+    contribs = [contrib_m1(docs[start:end, :], n_docs)
+                for start, end in equal_partitions(n_docs, n_partitions)]
 
+    return reduce(add, contribs)
+
+
+# ================ M2 Calculation ================
+def contrib_prod_e2_x(docs, test_x, n_docs):
+    ''' Compute contribution of the partition to the product of E2 and X
+
+    Parameters
+    -----------
+    docs : m-by-vocab_size array or csr_matrix
+        Current partition of word count vectors.
+    test_x : vocab_size-by-k array
+        Test matrix where k is the number of factors.
+    n_docs : int
+        Total number of documents, could be greater than m.
+
+    Returns
+    ----------
+    out : vocab_size-by-k array
+        Contribution of the partition to the product of E2 and X.
     '''
-    prod_E2_X = mult_E2_X(docs, X, n_docs)
-    return mult_M2_X_helper(prod_E2_X, X, M1, alpha0)
+    partition_size, vocab_size = docs.shape
+    _vocab_size, num_factors = test_x.shape
+    assert partition_size >= 1 and vocab_size >= 1
+    assert partition_size <= n_docs
+    assert vocab_size == _vocab_size and num_factors >= 1
 
-def whiten_E3(docs, W, n_docs):
-    ''' Whiten the E3 by W
+    document_lengths = np.squeeze(np.array(docs.sum(axis=1)))
+    diag_l = spsp.diags(1.0 / document_lengths / (document_lengths - 1))
 
+    scaled_docs = diag_l.dot(docs)
+    prod_x = scaled_docs.T.dot(docs.dot(test_x))
+
+    sum_scaled_docs = np.squeeze(np.array(scaled_docs.sum(axis=0)))
+    prod_x_adj = spsp.diags(sum_scaled_docs).dot(test_x)
+
+    return (prod_x - prod_x_adj) / n_docs
+
+def prod_m2_x(docs, test_x, alpha0, n_partitions=1):
+    ''' Compute the product of M2 by test matrix X over partitions of documents
+
+    Compute over partitions of documents in local mode, mostly
+    used for test purpose.
+
+    Parameters
+    -----------
+    docs : n_docs-by-vocab_size array
+        Entire collection of word count vectors.
+    test_x : vocab_size-by-k array
+        Test matrix where k is the number of factors.
+    alpha0 : float
+        Sum of the Dirichlet prior parameter.
+    n_partitions : int, optional
+        Number of partitions, 1 by default.
+
+    Returns
+    -----------
+    out : vocab_size-by-k array
+        Product of M2 by X.
     '''
-    m, _ = docs.shape
-    _, k = W.shape
+    def reduce_and_adjust(contribs, docs_m1, test_x, alpha0):
+        ''' Reduce contributions and adjust '''
+        adj = alpha0 / (alpha0 + 1) * np.outer(docs_m1, docs_m1.dot(test_x))
+        return reduce(add, contribs) - adj
+
+    n_docs, vocab_size = docs.shape
+    _vocab_size, num_factors = test_x.shape
+    assert n_docs >= 1 and vocab_size >= 1
+    assert n_partitions >= 1 and n_partitions <= n_docs
+    assert vocab_size == _vocab_size and num_factors >= 1
+    assert alpha0 > 0
+
+    contribs = [contrib_prod_e2_x(docs[start:end, :], test_x, n_docs)
+                for start, end in equal_partitions(n_docs, n_partitions)]
+
+    return reduce_and_adjust(contribs, moment1(docs, n_partitions),
+                             test_x, alpha0)
+
+
+# ============== M3 calculation ================
+def contrib_whiten_e3(docs, whn, n_docs):
+    ''' Compute contribution of the partition to whitening E3
+
+    Parameters
+    -----------
+    docs : m-by-vocab_size array or csr_matrix
+        Current partition of word count vectors.
+    whn : vocab_size-by-k array
+        Whitening matrix.
+    n_docs : int
+        Total number of documents, could be greater than m.
+
+    Returns
+    ----------
+    out : k-by-(k ** 2) array
+        Contribution to the whitened E3, unfolded version.
+    '''
+    # pylint: disable=too-many-locals
+    partition_size, vocab_size = docs.shape
+    _vocab_size, num_factors = whn.shape
+    assert partition_size >= 1 and vocab_size >= 1
+    assert vocab_size == _vocab_size and num_factors >= 1
+    assert partition_size <= n_docs
 
     # m-by-k
-    p = docs.dot(W)
+    whitened_docs = docs.dot(whn)
 
     # length-m
-    l = np.squeeze(np.array(docs.sum(axis=1)))
-    diag_l = spsp.diags(1.0 / l / (l - 1) / (l - 2))
+    document_lengths = np.squeeze(np.array(docs.sum(axis=1)))
+    diag_l = spsp.diags(1.0 / document_lengths
+                        / (document_lengths - 1) / (document_lengths - 2))
 
     # m-by-k
     scaled_docs = diag_l.dot(docs)
-    scaled_p = diag_l.dot(p)
+    scaled_whitened_docs = diag_l.dot(whitened_docs)
+
     # V-by-k
-    r = scaled_docs.T.dot(p)
-    vocab_size, _ = r.shape
+    # The 3rd vector for the tensor products w w rho and its
+    # permutations, where w is every row of the whitening matrix.
+    # We reduce over the current partition to obtain rho.
+    rho = scaled_docs.T.dot(whitened_docs)
+
     # length-V
-    r2 = np.squeeze(np.array(scaled_docs.sum(axis=0)))
+    # Coefficient vector before the tensor products w w w,
+    # where w is every row of the whitening matrix.
+    tau = np.squeeze(np.array(scaled_docs.sum(axis=0)))
 
     # ======== whiten E3 ========
+    # 1st-order terms
     # k-by-k-by-k
-    outer_p_p = np.einsum('ij,ik->ijk', p, p).reshape((m, -1))
-    t1 = scaled_p.T.dot(outer_p_p).flatten()
+    outer_p_p = (np.einsum('ij,ik->ijk', whitened_docs, whitened_docs)
+                 .reshape((partition_size, -1)))
+    terms1 = scaled_whitened_docs.T.dot(outer_p_p).flatten()
 
-    outer_w_w = np.einsum('ij,ik->ijk', W, W).reshape((vocab_size, -1))
-    outer_r_w = np.einsum('ij,ik->ijk', r, W).reshape((vocab_size, -1))
-    t21 = np.einsum('ij,ik->ijk', r, outer_w_w).reshape((vocab_size, -1))
-    t22 = np.einsum('ij,ik->ijk', W, outer_r_w).reshape((vocab_size, -1))
-    t23 = np.einsum('ij,ik->ijk', outer_w_w, r).reshape((vocab_size, -1))
+    # 2nd-order terms
+    outer_w_w = np.einsum('ij,ik->ijk', whn, whn).reshape((vocab_size, -1))
+    outer_r_w = np.einsum('ij,ik->ijk', rho, whn).reshape((vocab_size, -1))
+    t21 = np.einsum('ij,ik->ijk', rho, outer_w_w).reshape((vocab_size, -1))
+    t22 = np.einsum('ij,ik->ijk', whn, outer_r_w).reshape((vocab_size, -1))
+    t23 = np.einsum('ij,ik->ijk', outer_w_w, rho).reshape((vocab_size, -1))
 
-    t2 = t21.sum(axis=0) + t22.sum(axis=0) + t23.sum(axis=0)
+    terms2 = t21.sum(axis=0) + t22.sum(axis=0) + t23.sum(axis=0)
 
-    outer_w_3 = np.einsum('ij,ik,il->ijkl', W, W, W).reshape((vocab_size, -1))
-    t3 = 2 * r2.dot(outer_w_3)
+    # 3rd-order terms
+    outer_w_3 = (np.einsum('ij,ik,il->ijkl', whn, whn, whn)
+                 .reshape((vocab_size, -1)))
+    terms3 = 2 * tau.dot(outer_w_3)
 
-    whitened_E3 = (t1 - t2 + t3).reshape((k, k * k)) / n_docs
-    return whitened_E3
+    whitened_e3 = (terms1 - terms2 + terms3) / n_docs
+    return whitened_e3.reshape((num_factors, -1))
 
-def whiten_E2_M1(docs, W, n_docs, M1):
-    ''' Whiten the tensor products of E2 and M1 by W
+def contrib_whiten_e2m1(docs, docs_m1, whn, n_docs):
+    ''' Compute contribution of the partition to whitening the tensor products of E2 and M1
 
+    Parameters
+    -----------
+    docs : m-by-vocab_size array or csr_matrix
+        Current partition of word count vectors.
+    docs_m1 : length-vocab_size array
+        M1 (average word count vector) of the entire collection
+        of word count vectors.
+    whn : vocab_size-by-k array
+        Whitening matrix.
+    n_docs : int
+        Total number of documents, could be greater than m.
+
+    Returns
+    ----------
+    out : k-by-(k ** 2) array
+        Contribution to the whitened tensor products of E2 and M1,
+        unfolded version.
     '''
-    m, _ = docs.shape
-    _, k = W.shape
+    # pylint: disable=too-many-locals
+    partition_size, vocab_size = docs.shape
+    _vocab_size, num_factors = whn.shape
+    assert partition_size >= 1 and vocab_size >= 1
+    assert n_docs >= partition_size
+    assert vocab_size == _vocab_size and num_factors >= 1
 
     # m-by-k
-    p = docs.dot(W)
+    whitened_docs = docs.dot(whn)
     # length-k
-    q = M1.dot(W)
+    whitened_m1 = docs_m1.dot(whn)
+    tiled_whitened_m1 = np.tile(whitened_m1, [partition_size, 1])
 
     # length-m
-    l = np.squeeze(np.array(docs.sum(axis=1)))
-    diag_l = spsp.diags(1.0 / l / (l - 1))
+    document_lengths = np.squeeze(np.array(docs.sum(axis=1)))
+    diag_l = spsp.diags(1.0 / document_lengths / (document_lengths - 1))
 
     # m-by-V
     scaled_docs = diag_l.dot(docs)
     # m-by-k
-    scaled_p = diag_l.dot(p)
-    # V-by-k
-    r = np.outer(scaled_docs.sum(axis=0), q)
-    vocab_size, _ = r.shape
+    scaled_whitened_docs = diag_l.dot(whitened_docs)
 
-    tiled_q = np.tile(q, [m, 1])
+    # V-by-k
+    # The 3rd vector to make tensor products w w rho and its
+    # permutations, where w is every row of the whitening matrix.
+    # We reduce over the partition to obtain r1.
+    rho = np.outer(scaled_docs.sum(axis=0), whitened_m1)
 
     # ====== whiten E2_M1 =========
-    outer_scaled_p_p = np.einsum('ij,ik->ijk', scaled_p, p).reshape((m, -1))
-    outer_q_scaled_p = (np.einsum('ij,ik->ijk', tiled_q, scaled_p)
-                        .reshape((m, -1)))
-    outer_w_w = np.einsum('ij,ik->ijk', W, W).reshape((vocab_size, -1))
-    outer_r_w = np.einsum('ij,ik->ijk', r, W).reshape((vocab_size, -1))
+    # 1st-order terms
+    outer_scaled_p_p = (np.einsum('ij,ik->ijk', scaled_whitened_docs,
+                                  whitened_docs)
+                        .reshape((partition_size, -1)))
+    outer_q_scaled_p = (np.einsum('ij,ik->ijk', tiled_whitened_m1,
+                                  scaled_whitened_docs)
+                        .reshape((partition_size, -1)))
 
-    u11 = np.einsum('ij,ik->ijk', tiled_q, outer_scaled_p_p).reshape((m, -1))
-    u12 = np.einsum('ij,ik->ijk', p, outer_q_scaled_p).reshape((m, -1))
-    u13 = np.einsum('ij,ik->ijk', outer_scaled_p_p, tiled_q).reshape((m, -1))
+    u11 = (np.einsum('ij,ik->ijk', tiled_whitened_m1, outer_scaled_p_p)
+           .reshape((partition_size, -1)))
+    u12 = (np.einsum('ij,ik->ijk', whitened_docs, outer_q_scaled_p)
+           .reshape((partition_size, -1)))
+    u13 = (np.einsum('ij,ik->ijk', outer_scaled_p_p, tiled_whitened_m1)
+           .reshape((partition_size, -1)))
 
-    u21 = np.einsum('ij,ik->ijk', r, outer_w_w).reshape((vocab_size, -1))
-    u22 = np.einsum('ij,ik->ijk', W, outer_r_w).reshape((vocab_size, -1))
-    u23 = np.einsum('ij,ik->ijk', outer_w_w, r).reshape((vocab_size, -1))
+    whitened = u11.sum(axis=0) + u12.sum(axis=0) + u13.sum(axis=0)
 
-    u1 = u11.sum(axis=0) + u12.sum(axis=0) + u13.sum(axis=0)
-    u2 = u21.sum(axis=0) + u22.sum(axis=0) + u23.sum(axis=0)
+    # 2nd-order terms
+    outer_w_w = np.einsum('ij,ik->ijk', whn, whn).reshape((vocab_size, -1))
+    outer_r_w = np.einsum('ij,ik->ijk', rho, whn).reshape((vocab_size, -1))
 
-    whitened_E2_M1 = ((u1 - u2) / n_docs).reshape((k, k * k))
-    return whitened_E2_M1
+    u21 = np.einsum('ij,ik->ijk', rho, outer_w_w).reshape((vocab_size, -1))
+    u22 = np.einsum('ij,ik->ijk', whn, outer_r_w).reshape((vocab_size, -1))
+    u23 = np.einsum('ij,ik->ijk', outer_w_w, rho).reshape((vocab_size, -1))
 
-def whiten_M3_helper(whitened_E3, whitened_E2_M1, W, M1, alpha0):
-    ''' Whiten M3 given whitened E3 and whitened tensor products of E2 and M1
+    whitened_adj = u21.sum(axis=0) + u22.sum(axis=0) + u23.sum(axis=0)
 
+    whitened_e2_m1 = (whitened - whitened_adj) / n_docs
+    return whitened_e2_m1.reshape((num_factors, -1))
+
+def whiten_m3(docs, whn, alpha0, n_partitions=1):
+    ''' Whiten M3
+
+    Compute over partition of documents in local mode, mostly used
+    for test purpose.
+
+    Parameters
+    -----------
+    docs : n_docs-by-vocab_size array or csr_matrix
+        Entire collection of word count vectors.
+    whn : vocab_size-by-k array
+        Whitening matrix.
+    alpha0 : float
+        Sum of Dirichlet prior parameter.
+    n_partitions : int, optional
+        Number of partitions, 1 by default.
+
+    Returns
+    ----------
+    out : k-by-(k ** 2) array
+        Whitened M3, unfolded version.
     '''
-    _, k = W.shape
-    q = M1.dot(W)
-    whitened_M1_3 = np.einsum('i,j,k->ijk', q, q, q).reshape((k, k * k))
+    def reduce_and_adjust(contribs_e3, contribs_e2m1, docs_m1, whn, alpha0):
+        ''' Reduce the contributions and adjust '''
+        _, num_factors = whn.shape
+        # length-k
+        whitened_m1 = docs_m1.dot(whn)
+        whitened_m1_3 = (np.einsum('i,j,k->ijk', whitened_m1, whitened_m1,
+                                   whitened_m1).reshape((num_factors, -1)))
 
-    whitened_M3 = (
-        whitened_E3
-        - alpha0 / (alpha0 + 2) * whitened_E2_M1
-        + 2 * alpha0 ** 2 / (alpha0 + 1) / (alpha0 + 2) * whitened_M1_3
-    )
+        coeff1 = alpha0 / (alpha0 + 2)
+        coeff2 = 2 * alpha0 ** 2 / (alpha0 + 1) / (alpha0 + 2)
+        return (reduce(add, contribs_e3)
+                - coeff1 * reduce(add, contribs_e2m1)
+                + coeff2 * whitened_m1_3)
 
-    return whitened_M3
+    n_docs, vocab_size = docs.shape
+    _vocab_size, num_factors = whn.shape
+    assert n_docs >= 1 and vocab_size >= 1
+    assert n_partitions <= n_docs
+    assert vocab_size == _vocab_size and num_factors >= 1
+    assert alpha0 > 0
 
-def whiten_M3(docs, W, n_docs, M1, alpha0):
-    ''' Whiten M3 in local mode
+    contribs_e3 = [contrib_whiten_e3(docs[start:end, :], whn, n_docs)
+                   for start, end in equal_partitions(n_docs, n_partitions)]
 
-    '''
-    whitened_E3 = whiten_E3(docs, W, n_docs)
-    whitened_E2_M1 = whiten_E2_M1(docs, W, n_docs, M1)
-    return whiten_M3_helper(whitened_E3, whitened_E2_M1,
-                            W, M1, alpha0)
+    docs_m1 = moment1(docs, n_partitions)
+    contribs_e2m1 = [contrib_whiten_e2m1(docs[start:end, :], docs_m1,
+                                         whn, n_docs)
+                     for start, end in equal_partitions(n_docs, n_partitions)]
+
+    return reduce_and_adjust(contribs_e3, contribs_e2m1, docs_m1,
+                             whn, alpha0)
--- test_cumulants.py
+++ test_cumulants.py
@@ -1,24 +1,21 @@
 ''' Test cumulants computations in local mode
 
 '''
+from functools import reduce
+from operator import add
 import numpy as np
 import scipy.sparse
 import scipy.stats
-from functools import reduce
-from operator import add
-from cumulants import *
+from cumulants import prod_m2_x, whiten_m3
 
 def simulate_word_count_vectors(alpha, beta, n_docs,
-                                min_total_word_count,
-                                max_total_word_count,
-                                vocab_size):
-    ''' Simulate a collection of word count vectors
-
-    '''
+                                min_total_word_count, max_total_word_count):
+    ''' Simulate a collection of word count vectors '''
     # Number of topics
     k = len(alpha)
+    vocab_size, _k = beta.shape
 
-    assert (vocab_size, k) == beta.shape
+    assert k == _k
     assert np.allclose(np.sum(beta, axis=0), 1)
     assert n_docs > 0 and vocab_size > 0
     assert max_total_word_count > min_total_word_count
@@ -41,28 +38,30 @@ def simulate_word_count_vectors(alpha, beta, n_docs,
     return word_count_vectors
 
 def compute_e2(word_count_vectors, n_docs):
-    ''' Compute E2 by summing the contribution of every document
+    ''' Compute E2 by summing the contribution of every document '''
+    def doc_contrib(word_count_vector):
+        ''' Compute contribution of the current document to E2 '''
+        total_count = np.sum(word_count_vector)
+        assert total_count >= 2
+        return ((np.outer(word_count_vector, word_count_vector)
+                 - np.diag(word_count_vector))
+                / total_count / (total_count - 1) / n_docs)
 
-    '''
-    def doc_contrib(wc):
-        l = np.sum(wc)
-        assert l >= 2
-        return (np.outer(wc, wc) - np.diag(wc)) / l / (l - 1) / n_docs
-
-    return reduce(add, [doc_contrib(wc) for wc in word_count_vectors])
+    return reduce(add, [doc_contrib(word_count_vector)
+                        for word_count_vector in word_count_vectors])
 
 def compute_e3(word_count_vectors, n_docs):
-    ''' Compute E3 by summing the contribution of every document
+    ''' Compute E3 by summing the contribution of every document '''
+    def doc_contrib(word_count_vector):
+        ''' Compute contribution of the current document to E3 '''
+        total_count = np.sum(word_count_vector)
+        assert total_count >= 3
+        contrib = np.einsum('i,j,k->ijk', word_count_vector,
+                            word_count_vector, word_count_vector)
 
-    '''
-    def doc_contrib(wc):
-        l = np.sum(wc)
-        assert l >= 3
-        contrib = np.einsum('i,j,k->ijk', wc, wc, wc)
-
-        for i, wc_i in enumerate(wc):
-            for j, wc_j in enumerate(wc):
-                for k, wc_k in enumerate(wc):
+        for i, wc_i in enumerate(word_count_vector):
+            for j, wc_j in enumerate(word_count_vector):
+                for k, wc_k in enumerate(word_count_vector):
                     if i == j:
                         contrib[i, j, k] -= wc_i * wc_k
                     if j == k:
@@ -72,58 +71,50 @@ def compute_e3(word_count_vectors, n_docs):
                     if i == j and j == k:
                         contrib[i, j, k] += 2 * wc_i
 
-        return contrib / l / (l - 1) / (l - 2) / n_docs
+        return (contrib / total_count / (total_count - 1)
+                / (total_count - 2) / n_docs)
 
-    return reduce(add, [doc_contrib(wc) for wc in word_count_vectors])
+    return reduce(add, [doc_contrib(word_count_vector)
+                        for word_count_vector in word_count_vectors])
 
 def compute_m1(word_count_vectors):
-    ''' Compute M1 i.e. the average of the normalised word count vectors
-
-    '''
+    ''' Compute M1 i.e. average of the normalised word count vectors '''
     normalized = word_count_vectors.T / np.sum(word_count_vectors, axis=1)
     return np.mean(normalized.T, axis=0)
 
 def compute_m2(word_count_vectors, alpha0, n_docs):
-    ''' Compute M2 by subtracting from E2 adjustment term related to M1
+    ''' Compute M2 '''
+    docs_m1 = compute_m1(word_count_vectors)
+    docs_e2 = compute_e2(word_count_vectors, n_docs)
 
-    '''
-    m1 = compute_m1(word_count_vectors)
-    e2 = compute_e2(word_count_vectors, n_docs)
-
-    return e2 - alpha0 / (alpha0 + 1) * np.outer(m1, m1)
+    return docs_e2 - alpha0 / (alpha0 + 1) * np.outer(docs_m1, docs_m1)
 
 def compute_m3(word_count_vectors, alpha0, n_docs):
-    ''' Compute M3 by subtracting from E3 adjustment terms related to M1 and E2
+    ''' Compute M3 '''
+    docs_m1 = compute_m1(word_count_vectors)
+    docs_e2 = compute_e2(word_count_vectors, n_docs)
+    docs_e3 = compute_e3(word_count_vectors, n_docs)
 
-    '''
-    m1 = compute_m1(word_count_vectors)
-    e2 = compute_e2(word_count_vectors, n_docs)
-    e3 = compute_e3(word_count_vectors, n_docs)
+    assert np.isclose(np.sum(docs_e3), 1)
 
-    assert np.isclose(np.sum(e3), 1)
+    adj_e2 = (np.einsum('ij,k->ijk', docs_e2, docs_m1)
+              + np.einsum('ij,k->ikj', docs_e2, docs_m1)
+              + np.einsum('ij,k->kij', docs_e2, docs_m1))
+    adj_m1 = np.einsum('i,j,k->ijk', docs_m1, docs_m1, docs_m1)
 
-    adj_e2 = (np.einsum('ij,k->ijk', e2, m1)
-              + np.einsum('ij,k->ikj', e2, m1)
-              + np.einsum('ij,k->kij', e2, m1))
-    adj_m1 = np.einsum('i,j,k->ijk', m1, m1, m1)
-
-    return (e3 - alpha0 / (alpha0 + 2) * adj_e2
+    return (docs_e3 - alpha0 / (alpha0 + 2) * adj_e2
             + 2 * alpha0 ** 2 / (alpha0 + 1) / (alpha0 + 2) * adj_m1)
 
-def contract_m3(m3, x):
-    ''' Contract each dimension of m3 by multiplying by x.T
-
-    '''
-    contracted_d0 = np.einsum('ij,jkl->ikl', x.T, m3)
-    contracted_d1 = np.einsum('ij,kjl->kil', x.T, contracted_d0)
-    contracted_d2 = np.einsum('ij,klj->kli', x.T, contracted_d1)
+def contract_m3(ts3, test_x):
+    ''' Contract each dimension of ts3 by multiplying by test_x.T '''
+    contracted_d0 = np.einsum('ij,jkl->ikl', test_x.T, ts3)
+    contracted_d1 = np.einsum('ij,kjl->kil', test_x.T, contracted_d0)
+    contracted_d2 = np.einsum('ij,klj->kli', test_x.T, contracted_d1)
 
     return contracted_d2
 
 def test_random_product_m2_x():
-    ''' Test product of M2 by random test matrix X
-
-    '''
+    ''' Test product of M2 by random test matrix X '''
     k = 10
     vocab_size = 100
     n_docs = 200
@@ -137,22 +128,18 @@ def test_random_product_m2_x():
 
     word_count_vectors = simulate_word_count_vectors(alpha, beta, n_docs,
                                                      min_total_word_count,
-                                                     max_total_word_count,
-                                                     vocab_size)
+                                                     max_total_word_count)
 
     alpha0 = np.sum(alpha)
-    m1 = compute_m1(word_count_vectors)
-    m2 = compute_m2(word_count_vectors, alpha0, n_docs)
+    docs_m2 = compute_m2(word_count_vectors, alpha0, n_docs)
 
-    x = np.random.randn(vocab_size, k)
-    prod_m2_x = mult_M2_X(word_count_vectors, x, n_docs, m1, alpha0)
+    test_x = np.random.randn(vocab_size, k)
+    m2x = prod_m2_x(word_count_vectors, test_x, alpha0)
 
-    assert np.linalg.norm(m2.dot(x) - prod_m2_x) <= 1e-6
+    assert np.linalg.norm(docs_m2.dot(test_x) - m2x) <= 1e-6
 
 def test_whiten_m3():
-    ''' Test whitening M3 by random test matrix X
-
-    '''
+    ''' Test whitening M3 by random test matrix X '''
     k = 5
     vocab_size = 20
     n_docs = 50
@@ -166,15 +153,13 @@ def test_whiten_m3():
 
     word_count_vectors = simulate_word_count_vectors(alpha, beta, n_docs,
                                                      min_total_word_count,
-                                                     max_total_word_count,
-                                                     vocab_size)
+                                                     max_total_word_count)
 
     alpha0 = np.sum(alpha)
-    m1 = compute_m1(word_count_vectors)
-    m3 = compute_m3(word_count_vectors, alpha0, n_docs)
+    docs_m3 = compute_m3(word_count_vectors, alpha0, n_docs)
 
-    x = np.random.randn(vocab_size, k)
-    whitened_m3 = (whiten_M3(word_count_vectors, x, n_docs, m1, alpha0)
+    test_x = np.random.randn(vocab_size, k)
+    whitened_m3 = (whiten_m3(word_count_vectors, test_x, alpha0)
                    .reshape((k, k, k)))
 
-    assert np.linalg.norm(contract_m3(m3, x) - whitened_m3) <= 1e-6
+    assert np.linalg.norm(contract_m3(docs_m3, test_x) - whitened_m3) <= 1e-6
