813bc727cb66f315b2737661673d419fab7584b2
Jencir Lee <jli05@icloud.com>
2017-05-14 11:15:29 +0100

    fix multi-processing

--- corpuses/dump_bow.py
+++ corpuses/dump_bow.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 from gensim.corpora.dictionary import Dictionary
 from gensim.corpora.wikicorpus import WikiCorpus
 
-def dump_bow(corpus, partition_size=10000, limit=200, output_prefix='dump'):
+def dump_bow(corpus, partition_size=50, limit=200, output_prefix='dump'):
     ''' Dump Bag-of-Word from gensim WikiCorpus
 
     Iterate through the documents in the wiki dump and dump the
@@ -18,8 +18,9 @@ def dump_bow(corpus, partition_size=10000, limit=200, output_prefix='dump'):
         The Wikidump corpus.
     partition_size: int
         Number of documents in each .txt.gz dump file.
-    limit: int
-        The total number of documents to dump.
+    limit: int or None
+        The total number of documents to dump, or all the documents
+        when it is None.
     output_prefix: str
         Prefix of the dump files.
     '''
@@ -28,6 +29,13 @@ def dump_bow(corpus, partition_size=10000, limit=200, output_prefix='dump'):
         fname = '{}-{:06d}.txt.gz'.format(output_prefix, partition_id)
         with gzip.open(fname, 'wt') as partition_file:
             partition_file.write(buf)
+
+    if limit is None:
+        limit = len(corpus)
+
+    assert partition_size >= 1 and limit >= 1
+    # gensim 2.0 requires this otherwise the multi-processing locks up
+    assert partition_size <= limit
 
     count_documents = 0
     partition_id = 0
@@ -38,12 +46,15 @@ def dump_bow(corpus, partition_size=10000, limit=200, output_prefix='dump'):
         buf += text + '\n'
         count_documents += 1
 
+        if count_documents % 200 == 0:
+            print('Processes {} documents.'.format(count_documents))
+
         if count_documents % partition_size == 0:
             write_buffer(buf, output_prefix, partition_id)
             buf = ''
             partition_id += 1
 
-        if limit is not None and count_documents >= limit:
+        if count_documents >= limit:
             break
 
     if buf:
@@ -65,7 +76,7 @@ def main():
                               'default: 50'))
     parser.add_argument('-l', '--limit', type=int, default=200,
                         help=('Total number of documents to dump, '
-                              '-1 for no limit, default: 200'))
+                              'or -1 for all documents, default: 200'))
     parser.add_argument('-o', '--output-prefix', type=str, default='dump',
                         help='Prefix of dump .txt.gz files, default: dump')
 
@@ -76,7 +87,7 @@ def main():
                       dictionary=wiki_dictionary)
 
     if args.limit == -1:
-        dump_bow(wiki, args.partition_size,
+        dump_bow(wiki, args.partition_size, None,
                  output_prefix=args.output_prefix)
         print('Dumped {} documents.'.format(len(wiki)))
     else:
